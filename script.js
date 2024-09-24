95a3a892fa27a6899baf68d1d5a3fea69d3aae3a
lucilapastore <lucilapastore@gmail.com>
2024-09-24 19:03:26 -0300

    feat:get the results of the round

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function getRandomComputerResult() {
   return options[randomIndex];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function hasPlayerWonTheRound(player, computer) {
   // Check winning conditions
   if (
@@ -20,3 +23,22 @@ function hasPlayerWonTheRound(player, computer) {
 
   return false; // Player loses or ties
 }
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  // Check if the player wins
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore += 1;
+    return `Player wins! ${userOption} beats ${computerResult} `;
+  } // Check for a tie
+  else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption} `;
+  } // If neither condition is met, the computer wins
+  else {
+    computerScore += 1;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
+
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
